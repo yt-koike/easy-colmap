085e5916422f11b9e0bed1b5dab741d9438cc097
yt-koike <y.koike.2001code@gmail.com>
2025-08-06 03:45:40 +0900

    20250806

--- data/exhaustive.sh
+++ data/exhaustive.sh
@@ -1,5 +1,5 @@
-mkdir /data/sparse/
+mkdir /data/sparse
 colmap feature_extractor --image_path /data/images --database_path /data/colmap.db
 colmap exhaustive_matcher --database_path /data/colmap.db
 colmap mapper --image_path /data/images --database_path /data/colmap.db --output_path /data/sparse/
-
+#colmap model_converter --input_path /data/sparse/0 --output_path /data/sparse_txt/ --output_type TXT
